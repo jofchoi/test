0ebb9b8309ab955478e4af7f6868f6da0ed82c86
jm <jofchoi@gmail.com>
2020-07-25 18:12:14 +0900

    예제 중하나. push 수정중

---
+++ tests.java
@@ -0,0 +1,13 @@
+package first;
+
+public class tests {
+
+	public static void main(String[] args) {
+		char ch1 = 'วั';
+		char ch2 = '\uD55C';
+		
+		System.out.println(ch1);
+		System.out.println(ch2);
+	}
+
+}
