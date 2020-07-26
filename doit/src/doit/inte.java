73489535d8c0461edff330409a12101d637d64d4
jm <jofchoi@gmail.com>
2020-07-26 23:39:16 +0900

    chapter3-1까지 예제 끝

---
+++ doit/src/doit/inte.java
@@ -0,0 +1,104 @@
+package doit;
+
+public class inte {
+
+	public static void main(String[] args) {
+		
+		/*
+		int num = 10;
+		System.out.println(+num);
+		System.out.println(-num);
+		System.out.println(num);
+		
+		num = -num;
+		
+		System.out.println(+num);
+		*/
+			
+		/*
+		int mathScore = 90;
+		int engScore = 70;
+		
+		int totalScore = mathScore + engScore;
+		System.out.println(totalScore);
+		
+		double aveScore = totalScore / 2.0 ;
+		System.out.println(aveScore);
+		*/
+		
+		
+		/*
+		int gameScore = 150;
+		
+		int lastScore1 = ++gameScore;
+		System.out.println(lastScore1);
+		
+		int lastScore2 = gameScore--;
+		System.out.println(lastScore2);
+		
+		
+		int myAge = 27;
+		boolean value = (myAge>25);
+		System.out.println(myAge);
+		
+		
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		boolean flag = (num1 >0) && (num2 >0);
+		System.out.println(flag);
+		
+		flag = (num1 < 0) && (num2 > 0);
+		System.out.println(flag);
+		
+		flag = (num1 < 0) || (num2 > 0);
+		System.out.println(flag);
+		
+				
+		int num1 = 10;
+		int i = 2;
+		
+		boolean value = ((num1 = num1 + 10) < 10) && ((i = i+2)< 10 );
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i);
+		
+		value = ((num1 = num1 + 10) > 10) || ((i = i+2)< 10 );
+		System.out.println(value);
+		System.out.println(num1);
+		System.out.println(i);
+		
+		
+		int num = 5;
+		int i = 10;
+		
+		boolean value = ((num = num * 10)>45) && ((i=i-5)<10);
+		
+	System.out.println(value);
+	System.out.println(num);
+	System.out.println(i);
+		
+	
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch;
+		ch = (fatherAge > motherAge)? 'T':'F';
+		
+		System.out.println(ch);
+		
+		*/
+		
+		int num = 10;
+		boolean isEven;
+		isEven = (num % 2 == 0) ? true:false;
+		
+		System.out.println(isEven);
+	
+		
+		
+		
+	}
+
+}
