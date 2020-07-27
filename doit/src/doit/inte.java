a05adcb8704924aed2c6dd15d74bc3fd408f93e3
jm <jofchoi@gmail.com>
2020-07-27 21:57:37 +0900

    if elseif 및 switch break 예제 업데이트

--- doit/src/doit/inte.java
+++ doit/src/doit/inte.java
@@ -154,9 +154,7 @@ public class inte {
 			System.out.println("입장료는 "+charge+"원 입니다.");
 		
 		
-	*/
-		
-		
+
 		int score =80;
 		char grade = 'F';
 				
@@ -181,9 +179,71 @@ public class inte {
 		}
 	
 		
+
+
+		int ranking = 1;
+		char medalcolor;
 		
 		
-	
+		switch(ranking){
+
+		case 1 : medalcolor = 'G';
+
+			break;
+
+		case 2 : medalcolor = 'S';
+
+			break;
+
+		case 3 : medalcolor ='B';
+
+			break;
+
+		default : medalcolor = 'A';
+		
+		}
+		
+		System.out.println(ranking +"등 메달의 색깔은 " + medalcolor + "입니다.");
+		
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	
 	
 	
